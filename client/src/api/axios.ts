ff59b606d26a229d2ede4cd253f4425e3fc0c07e
khalid-dev24 <khalidmisbaudeen04@gmail.com>
2025-07-08 18:22:58 +0100

    linked backend

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mentorship-platform-fullstack.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
